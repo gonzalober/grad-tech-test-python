d20c46c65ad3fad9a39cc647eaab940e3ecd8d5b
Charlotte Levin <charlotte.levin@ecs-digital.co.uk>
2021-08-02 10:50:23 +0100

    feat: python test

---
+++ python/README.md
@@ -0,0 +1,14 @@
+# Grad Tech Test - Python
+
+## Clone this repository
+please do not fork it!
+
+## Setup
+
+Install `python3` and matching `pip` on your system, use pip to install `pytest`.
+
+## Challenge
+
+In the file [medals.py](medals.py), there's a function for creating a medal table. Update this function to be able to process the sample input.
+
+Run the tests using `pytest medals.py`
---
+++ python/medals.py
@@ -0,0 +1,40 @@
+medalResults = [
+    {
+        "sport": "cycling",
+        "podium": ["1.China", "2.Germany", "3.ROC"]
+    },
+    {
+        "sport": "fencing",
+        "podium": ["1.ROC", "2.France", "3.Italy"]
+    },
+    {
+        "sport": "high jump",
+        "podium": ["1.Italy", "1.Qatar", "3.Belarus"]
+    },
+    {
+        "sport": "swimming",
+        "podium": ["1.USA", "2.France", "3.Brazil"]
+    }
+]
+
+def createMedalTable(results):
+    # Use the results object above to create a medal table
+    # The winner gets 3 points, second place 2 points and third place 1 point
+    return
+
+
+def test_function():
+    #This it the test function, please don't change me
+    medalTable = createMedalTable(medalResults)
+    expectedTable = {
+        "Italy": 4,
+        "France": 4,
+        "ROC": 4,
+        "USA": 3,
+        "Qatar": 3,
+        "China": 3,
+        "Germany": 2,
+        "Brazil": 1,
+        "Belarus": 1,
+    }
+    assert medalTable == expectedTable
